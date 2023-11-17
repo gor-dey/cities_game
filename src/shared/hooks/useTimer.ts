9611808faafa72678dee39238473f6f764ef98f6
gor-dey <gordeychepikov@gmail.com>
2023-11-18 05:11:17 +0600

    correcting timmer

--- src/shared/hooks/useTimer.ts
+++ src/shared/hooks/useTimer.ts
@@ -9,20 +9,36 @@ export const useTimer = ({
   initialSeconds,
   onTimerEnd,
 }: UseTimerProps): number => {
-  const [seconds, setSeconds] = useState(initialSeconds);
+  const [startTime, setStartTime] = useState<number | null>(null);
+
+  const calculateRemainingTime = (): number => {
+    if (startTime === null) return initialSeconds * 1000;
+
+    const elapsedTime = Date.now() - startTime;
+    const remainingTime = initialSeconds * 1000 - elapsedTime;
+
+    return Math.max(remainingTime, 0);
+  };
+
+  const [milliseconds, setMilliseconds] = useState(calculateRemainingTime());
 
   useEffect(() => {
+    if (startTime === null) {
+      setStartTime(Date.now());
+    }
+
     const timerInterval = setInterval(() => {
-      setSeconds((prevSeconds) => (prevSeconds > 0 ? prevSeconds - 1 : 0));
+      setMilliseconds(calculateRemainingTime());
     }, 1000);
 
-    if (seconds === 0) {
+    if (milliseconds === 0) {
       clearInterval(timerInterval);
       onTimerEnd && onTimerEnd();
     }
 
     return () => clearInterval(timerInterval);
-  }, [seconds, onTimerEnd]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [milliseconds, onTimerEnd, startTime]);
 
-  return seconds;
+  return Math.ceil(milliseconds / 1000);
 };
